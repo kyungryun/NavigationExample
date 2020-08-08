6db1ded5ca1a0e87d2cad4df4a506a0f06320a99
kyungryun <kyung_ryun@naver.com>
2020-08-08 15:02:57 +0900

    navigation example

--- app/src/main/java/com/navigationexample/MainActivity.kt
+++ app/src/main/java/com/navigationexample/MainActivity.kt
@@ -5,23 +5,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+    var Back_Key_Before_Time: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment,R.id.searchFragment,R.id.myPageFragment,R.id.menuFragment)
+        )
         navController = nav_host_fragment.findNavController()
-
-
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id){
+                R.id.homeFragment, R.id.searchFragment, R.id.myPageFragment, R.id.menuFragment -> showBotNav()
+                else -> hideBotNav()
+            }
+        }
         bottom_nav_bar.setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+        if(appBarConfiguration.topLevelDestinations.contains(navController.currentDestination?.id)){
+            if(System.currentTimeMillis() - Back_Key_Before_Time < 2000){
+                finish()
+            }else{
+                Toast.makeText(application, "Exit?", Toast.LENGTH_SHORT).show()
+                Back_Key_Before_Time = System.currentTimeMillis()
+            }
+            return;
+        }
+        super.onBackPressed()
+    }
+    private fun hideBotNav() {
+        bottom_nav_bar.visibility = View.GONE
+    }
+
+    private fun showBotNav() {
+        bottom_nav_bar.visibility = View.VISIBLE
     }
 
 }
---
+++ app/src/main/java/com/navigationexample/fragment/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.navigationexample.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.navigationexample.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        login_btn.setOnClickListener {
+            navController.navigate(R.id.action_to_login)
+        }
+    }
+
+}
