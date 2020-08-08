aee44bc3c06813ea3ed4528fc98cb64f5a014eee
kyungryun <kyung_ryun@naver.com>
2020-08-08 12:14:06 +0900

    navigation example

--- app/src/main/java/com/navigationexample/MainActivity.kt
+++ app/src/main/java/com/navigationexample/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.navigationexample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = nav_host_fragment.findNavController()
+
+
+        bottom_nav_bar.setupWithNavController(navController)
     }
+
 }
